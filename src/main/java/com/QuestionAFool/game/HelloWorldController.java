e8b8b7b936ce5d4fecb1c5eef2ebe456fa5f4632
Shoaib siddiquie M A <shoaib.m@imimobile.com>
2020-04-12 21:07:24 +0530

    samll lig changes

--- src/main/java/com/QuestionAFool/game/HelloWorldController.java
+++ src/main/java/com/QuestionAFool/game/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@GetMapping("/")
 	public String hello() {
-		return "Hello World ----------psqllllll--";
+		return "Hello World ----------psqllllllddd--";
 	}
 }
